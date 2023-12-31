72ed37bfa3c1a0bde34ac5b0f08ca1df96f557ad
sajith p j <sajith@vass.co.in>
2023-12-31 20:38:12 +0530

    vercel fix

--- src/components/Table/DataTablePagination.jsx
+++ src/components/Table/DataTablePagination.jsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import ChevronDown from "../icons/ChevronDown";
+import ChevronDown from "../Icons/ChevronDown";
 import ChevronForward from "../Icons/ChevronForward";
 
 
